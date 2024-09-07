cad8980785a3b32abc155cbe59ffcc555b7ad8af
James Dillon <111654755+JamesDillonDev@users.noreply.github.com>
2024-09-07 14:25:02 +0000

    Changed paths

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start the Python script in the background
-python3 /Projects/NodeLink/main.py &
+python3 ~/Projects/NodeLink/main.py
 
 # Wait for the Python script to start
 sleep 10
 
 # Navigate to the webview directory and run npm
-cd /Projects/NodeLink/webview
+cd ~/Projects/NodeLink/webview
 npm run start
